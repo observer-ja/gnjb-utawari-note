4bf9a6d79e26e3a6b3d3c8120276eef90323c220
observer-ja <observer.ja@gmail.com>
2026-07-08 17:52:55 +0900

    ライブデータ

---
+++ src/data/lives.js
@@ -0,0 +1,84 @@
+export const lives = [
+  /* 2019年 */
+  { id: "live2019_01", name: "バトボライブ2019", date: "2019-07-07", type: "fes" },
+  { id: "live2019_02", name: "SHIBUYA SUMMIT 2019", date: "2019-07-15", type: "fes" },
+  { id: "live2019_03", name: "CBCラジオ夏まつり2019", date: "2019-07-28", type: "fes" },
+  { id: "live2019_04", name: "エビライ2019", date: "2019-08-28", type: "fes" },
+  { id: "live2019_05", name: "2019RABまつり", date: "2019-09-07", type: "fes" },
+  { id: "live2019_06", name: "組長祭2019", date: "2019-09-08", type: "fes" },
+  { id: "live2019_07", name: "BOYS-ZONE Fes.2019", date: "2019-09-22", type: "fes" },
+  { id: "live2019_08", name: "ABSまつり2019", date: "2019-10-05", type: "fes" },
+  { id: "live2019_09", name: "げんじぶ物語", date: "2019-10-29", type: "oneman" },
+  { id: "live2019_10", name: "だんぜん!!FES extra", date: "2019-11-16", type: "fes" },
+
+  /* 2020年 */
+  { id: "live2020_01", name: "仮想げんじぶ空間", date: "2020-07-12", type: "stream" },
+  { id: "live2020_02", name: "仮想げんじぶ空間:case.2", date: "2020_08_16", type: "stream" },
+  { id: "live2020_03", name: "a-nation online 2020", date: "2020-08-29", type: "fes" },
+  { id: "live2020_04", name: "組長祭2020", date: "2020-09-22", type: "fes" },
+  { id: "live2020_05", name: "仮装げんじぶ空間", date: "2020_10-24", type: "stream" },
+
+  /* 2021年 */
+  { id: "live2021_01", name: "仮想げんじぶ空間:case.3", date: "2021-01-23", type: "stream" },
+  { id: "live2021_02", name: "げんじぶ空間:case.1", date: "2021-04-02", type: "oneman" },
+  { id: "live2021_03", name: "BREAK OUT祭", date: "2021-07-09", type: "fes" },
+  { id: "live2021_04", name: "NEOげんじぶ空間：α", date: "2021-07-10", type: "oneman" },
+  { id: "live2021_05", name: "フェクライ2021 AW", date: "2021-09-11", type: "fes" },
+  { id: "live2021_06", name: "第2回スター☆オーディション", date: "2021-11-07", type: "fes" },
+  { id: "live2021_07", name: "げんじぶ空間:case.2", date: "2021-12-27", type: "oneman" },
+
+  /* 2022年 */
+  { id: "live2022_01", name: "フェクライ2022 NY", date: "2022-01-21", type: "fes" },
+  { id: "live2022_02", name: "Tune Live 2022", date: "2022-03-02", type: "fes" },
+  { id: "live2022_03", name: "げんじぶ空間:case.3", date: "2022-03-28", type: "oneman" },
+  { id: "live2022_04", name: "私立げんじぶ学園", date: "2022-07-07", type: "oneman" },
+  { id: "live2022_05", name: "エビライ2022", date: "2022-08-20", type: "fes" },
+  { id: "live2022_06", name: "げんじぶ空間:case.4", date: "2022-09-04", type: "oneman" },
+  { id: "live2022_07", name: "フェクライ2022 SMR", date: "2022-10-12", type: "fes" },
+  { id: "live2022_08", name: "仮装げんじぶ空間:case.2", date: "2022-10-31", type: "stream" },
+  { id: "live2022_09", name: "東海大学建学祭", date: "2022-11-02", type: "fes" },
+  { id: "live2022_10", name: "ビヨフェス2022", date: "2022-11-03", type: "fes" },
+  { id: "live2022_11", name: "first observation", date: "2022-12-04", type: "oneman" },
+
+  /* 2023年 */
+  { id: "live2023_01", name: "サンリオフェス2023", date: "2023-01-21", type: "fes" },
+  { id: "live2023_02", name: "げんじぶ空間:case.5", date: "2023-01-28", type: "oneman" },
+  { id: "live2023_03", name: "SMASH BEAT", date: "2023-01-31", type: "fes" },
+  { id: "live2023_04", name: "第2回スポーツマンヒップ", date: "2023-03-25", type: "fes" },
+  { id: "live2023_05", name: "-Loud&Shuffle-", date: "2023-04-14", type: "oneman" },
+  { id: "live2023_06", name: "エビパレ2023", date: "2023-04-29", type: "fes" },
+  { id: "live2023_07", name: "ヘッドフォンを外して vol.3", date: "2023-06-01", type: "fes" },
+  { id: "live2023_08", name: "天の川学園", date: "2023-07-07", type: "oneman" },
+  { id: "live2023_09", name: "-G=ø-", date: "2023-07-19", type: "oneman" },
+  { id: "live2023_10", name: "サマステ2023", date: "2023-08-01", type: "fes" },
+  { id: "live2023_11", name: "エビライ2023", date: "2023-08-11", type: "fes"},
+  { id: "live2023_12", name: "イナロク2023", date: "2023-10-09", type: "fes" },
+  { id: "live2023_13", name: "Livejack 2023", date: "2023-11-03", type: "fes" },
+  { id: "live2023_14", name: "因果律の逆転", date: "2023-11-05", type: "oneman" },
+  { id: "live2023_15", name: "ゲンゲンゲンジブ", date: "2023-12-22", type: "oneman" },
+
+  /* 2024年 */
+  { id: "live2024_01", name: "架空のアウトライン", date: "2024-03-23", type: "oneman"},
+  { id: "live2024_02", name: "NYLON 20TH ANNIVERSARY PARTY", date: "2024-04-19", type: "fes"},
+  { id: "live2024_03", name: "夢現の続き", date: "2024-07-07", type: "oneman"},
+  { id: "live2024_04", name: "エビライ2024", date: "2024-08-17", type: "fes"},
+  { id: "live2024_05", name: "白昼夢への招待", date: "2024-11-17", type: "oneman"},
+  { id: "live2024_06", name: "Livejack 2024", date: "2024-11-23", type: "fes"},
+  { id: "live2024_07", name: "クリライ2024", date: "2024-12-23", type: "oneman"},
+
+  /* 2025年 */
+  { id: "live2025_01", name: "ENDRECHERI FES", date: "2025-02-24", type: "fes"},
+  { id: "live2025_02", name: "嘲笑倫理学のすゝめ", date: "2025-03-15", type: "oneman"},
+  { id: "live2025_03", name: "序破急", date: "2025-07-12", type: "oneman"},
+  { id: "live2025_04", name: "アスパ2025", date: "2025-08-30", type: "fes"},
+  { id: "live2025_05", name: "ガルアワ2025AW", date: "2025-10-18", type: "fes"},
+  { id: "live2025_06", name: "GFEST.2025", date: "2025-11-23", type: "fes"},
+  { id: "live2025_07", name: "Laboratory", date: "2025-12-23", type: "oneman"},
+
+  /* 2026年 */
+  { id: "live2026_01", name: "輪廻の箱庭", date:"2026-03-17", type: "oneman"},
+  { id: "live2026_02", name: "SMEX2026", date:"2026-04-18", type: "fes"},
+  { id: "live2026_03", name: "仮ノ現", date:"2026-06-27", type: "oneman"},
+  { id: "live2026_04", name: "原因は君にもある。", date:"2026-07-07", type: "oneman"},
+  { id: "live2026_05", name: "JOIN ALIVE", date:"2026-07-19", type: "fes"},
+];
