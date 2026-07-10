80b6ad8d05ba84f58dd429792ae4932b22caf6cb
observer-ja <observer.ja@gmail.com>
2026-07-10 16:13:32 +0900

    index.js作成

---
+++ src/data/index.js
@@ -0,0 +1,128 @@
+/* 楽曲データ */
+
+export const songsIndex = [
+    /* BATTLE STREET時代 */
+    {
+        id: "bs001",
+        title: "STARGAZER",
+        date: "2018-12-19",
+        cd: ["bs_01"],
+        cd_track: { "bs_01": 1 },
+        composer: "J.A.E",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        live_history: null,
+    },
+    {
+        id: "bs002",
+        title: "Voice",
+        date: "2018-12-19",
+        cd: ["bs_01"],
+        cd_track: { "bs_01": 2 },
+        composer: "NAMELESS AKA NL",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        live_history: null,
+    },
+    {
+        id: "bs003",
+        title: "Macanas.",
+        date: "2018-12-19",
+        cd: ["bs_01"],
+        cd_track: { "bs_01": 3 },
+        composer: "Yocke",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        live_history: null,
+    },
+    {
+        id: "bs004",
+        title: "Show Time!!",
+        date: "2019-06-19",
+        cd: ["bs_02"],
+        cd_track: { "bs_02": 1 },
+        composer: "Yocke",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        live_history: null,
+    },
+    {
+        id: "bs005",
+        title: "COUNTDOWN",
+        date: "2019-06-19",
+        cd: ["bs_02"],
+        cd_track: { "bs_02": 2 },
+        composer: "楊慶豪",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        live_history: null,
+    },
+    {
+        id: "bs006",
+        title: "Burn Burn Bomb",
+        date: "2019-06-19",
+        cd: ["bs_02"],
+        cd_track: { "bs_02": 3 },
+        composer: "浅利進吾",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        live_history: null,
+    },
+    {
+        id: "bs007",
+        title: "Like a Vampire",
+        date: "2019-06-19",
+        cd: ["bs_02"],
+        cd_track: { "bs_02": 4 },
+        composer: "朴優尊",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        live_history: null,
+    },
+
+    /* 2019年 */
+
+    /* 2020年 */
+
+    /* 2021年 */
+
+    /* 2022年 */
+
+    /* 2023年 */
+
+    /* 2024年 */
+
+    /* 2025年 */
+
+    /* 2026年 */
+
+    /* ひな形
+    {
+        id: "song",
+        title: "",
+        date: "20",
+        cd: [""],
+        cd_track: { "":  },
+        composer: "",
+        tieup: ,
+        tieup_info: ,
+        call: ,
+        mv: ,
+        live_history: ,
+    },
+    */
+]
