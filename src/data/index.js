1f74cb33e735c8a2c553864be9fbdeb495e18626
observer-ja <observer.ja@gmail.com>
2026-07-12 11:53:19 +0900

    2020年までの楽曲情報

--- src/data/index.js
+++ src/data/index.js
@@ -5,6 +5,7 @@ export const songsIndex = [
     {
         id: "bs001",
         title: "STARGAZER",
+        title_kana: "zzz_stargazer",
         date: "2018-12-19",
         cd: ["bs_01"],
         cd_track: { "bs_01": 1 },
@@ -13,11 +14,18 @@ export const songsIndex = [
         tieup_info: null,
         call: false,
         mv: false,
+        links: {
+            mv: null,
+            spotify: "https://open.spotify.com/track/5ok1L8JoVDUy8rirbbIeQJ",
+            apple_music: "https://music.apple.com/jp/song/stargazer/1466685737",
+            line_music: "https://music.line.me/webapp/track/mt000000000f39c22c",
+        },
         live_history: [],
     },
     {
         id: "bs002",
         title: "Voice",
+        title_kana: "zzz_voice",
         date: "2018-12-19",
         cd: ["bs_01"],
         cd_track: { "bs_01": 2 },
@@ -26,11 +34,18 @@ export const songsIndex = [
         tieup_info: null,
         call: false,
         mv: false,
+        links: {
+            mv: null,
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
         live_history: [],
     },
     {
         id: "bs003",
         title: "Macanas.",
+        title_kana: "zzz_macanas",
         date: "2018-12-19",
         cd: ["bs_01"],
         cd_track: { "bs_01": 3 },
@@ -39,11 +54,18 @@ export const songsIndex = [
         tieup_info: null,
         call: false,
         mv: false,
+        links: {
+            mv: null,
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
         live_history: [],
     },
     {
         id: "bs004",
         title: "Show Time!!",
+        title_kana: "zzz_showtime",
         date: "2019-06-19",
         cd: ["bs_02"],
         cd_track: { "bs_02": 1 },
@@ -52,11 +74,18 @@ export const songsIndex = [
         tieup_info: null,
         call: false,
         mv: false,
+        links: {
+            mv: null,
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
         live_history: [],
     },
     {
         id: "bs005",
         title: "COUNTDOWN",
+        title_kana: "zzz_countdown",
         date: "2019-06-19",
         cd: ["bs_02"],
         cd_track: { "bs_02": 2 },
@@ -65,11 +94,18 @@ export const songsIndex = [
         tieup_info: null,
         call: false,
         mv: false,
+        links: {
+            mv: null,
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
         live_history: [],
     },
     {
         id: "bs006",
         title: "Burn Burn Bomb",
+        title_kana: "zzz_burnburnbomb",
         date: "2019-06-19",
         cd: ["bs_02"],
         cd_track: { "bs_02": 3 },
@@ -78,11 +114,18 @@ export const songsIndex = [
         tieup_info: null,
         call: false,
         mv: false,
+        links: {
+            mv: null,
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
         live_history: [],
     },
     {
         id: "bs007",
         title: "Like a Vampire",
+        title_kana: "zzz_likeavampire",
         date: "2019-06-19",
         cd: ["bs_02"],
         cd_track: { "bs_02": 4 },
@@ -91,12 +134,270 @@ export const songsIndex = [
         tieup_info: null,
         call: false,
         mv: false,
+        links: {
+            mv: null,
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
         live_history: [],
     },
 
     /* 2019年 */
+    {
+        id: "song001",
+        title: "原因は自分にある。",
+        title_kana: "げんいんはじぶんにある",
+        date: "2019-10-09",
+        cd: ["sg_01", "al_01"],
+        cd_track: { "sg_01": 1 , "al_01": 9 },
+        composer: "久下真音",
+        tieup: true,
+        tieup_info: [
+            "テレビ朝日「お願い！ランキング」9月度EDテーマソング",
+            "AbemaTV「Abema Prime」10月度EDテーマソング"
+        ],
+        call: false,
+        mv: true,
+        links: {
+            mv: "https://www.youtube.com/watch?v=-9lpqo83-AA",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            { live_id: "live2026_04", position: 1, note: "" },
+            { live_id: "live2026_03", position: 1, note: "" },
+            { live_id: "live2026_02", position: 2, note: "" },
+
+            { live_id: "live2025_08", position: 5, note: "" },
+            { live_id: "live2025_05", position: 2, note: "" },
+            { live_id: "live2025_02", position: 18, note: "メドレー" },
+
+        ],
+    },
+    {
+        id: "song002",
+        title: "ギミギミラブ",
+        title_kana: "ぎみぎみらぶ",
+        date: "2019-10-09",
+        cd: ["sg_01"],
+        cd_track: { "sg_01": 2 },
+        composer: "久下真音",
+        tieup: false,
+        tieup_info: null,
+        call: true,
+        mv: null,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song003",
+        title: "Up  and Down",
+        title_kana: "zzz_upanddown",
+        date: "2019-10-09",
+        cd: ["sg_01"],
+        cd_track: { "sg_01": 3 },
+        composer: "朴優尊",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song004",
+        title: "ラベンダー",
+        title_kana: "らべんだー",
+        date: "2019-10-09",
+        cd: ["sg_01"],
+        cd_track: { "sg_01": 4 },
+        composer: "朴優尊",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
 
     /* 2020年 */
+    {
+        id: "song005",
+        title: "嗜好に関する世論調査",
+        title_kana: "しこうにかんするよろんちょうさ",
+        date: "2020-01-22",
+        cd: ["sg_02", "al_01"],
+        cd_track: { "sg_02": 1 , "al_01": 2 },
+        composer: "久下真音",
+        tieup: true,
+        tieup_info: ["テレビ朝日系「BREAK OUT」2020年1月度OPテーマソング"],
+        call: true,
+        mv: true,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song006",
+        title: "Joy to the world",
+        title_kana: "zzz_joytotheworld",
+        date: "2020-01-22",
+        cd: ["sg_02"],
+        cd_track: { "sg_02": 2 },
+        composer: "久下真音",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: false,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song007",
+        title: "ジュトゥブ",
+        title_kana: "じゅとぅぶ",
+        date: "2020-01-22",
+        cd: ["sg_02"],
+        cd_track: { "sg_02": 3 },
+        composer: "100回嘔吐",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: true,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song008",
+        title: "時速3km",
+        title_kana: "じそくさんきろ",
+        date: "2020-01-22",
+        cd: ["sg_02"],
+        cd_track: { "sg_02": 4 },
+        composer: "100回嘔吐",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: null,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song009",
+        title: "嘘から始まる自称系",
+        title_kana: "うそからはじまるじしょうけい",
+        date: "2020-04-11",
+        cd: ["al_01"],
+        cd_track: { "al_01": 3 },
+        composer: "久下真音",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: true,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song010",
+        title: "シェイクスピアに学ぶ恋愛定理",
+        title_kana: "しぇいくすぴあにまなぶれんあいていり",
+        date: "2020-07-12",
+        cd: ["al_01"],
+        cd_track: { "al_01": 7 },
+        composer: "久下真音",
+        tieup: true,
+        tieup_info: ["MBS／TBS系ドラマ「年の差婚」OPテーマソング"],
+        call: false,
+        mv: true,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
+    {
+        id: "song011",
+        title: "幽かな夜の夢",
+        title_kana: "かすかなよのゆめ",
+        date: "2020-10-24",
+        cd: ["al_01"],
+        cd_track: { "al_01": 8 },
+        composer: "久下真音",
+        tieup: false,
+        tieup_info: null,
+        call: false,
+        mv: true,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
+    },
 
     /* 2021年 */
 
@@ -114,6 +415,7 @@ export const songsIndex = [
     {
         id: "song",
         title: "",
+        title_kana: "",
         date: "20",
         cd: [""],
         cd_track: { "":  },
@@ -122,7 +424,17 @@ export const songsIndex = [
         tieup_info: ,
         call: ,
         mv: ,
-        live_history: ,
+        links: {
+            mv: "",
+            spotify: "",
+            apple_music: "",
+            line_music: "",
+        },
+        live_history: [
+            
+        ],
     },
+    ライブ情報は後でまとめて書く
+    外部リンクも
     */
 ]
