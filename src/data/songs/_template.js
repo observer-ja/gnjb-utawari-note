51185c8672b951fc283e9351134094d21f91c2b6
observer-ja <observer.ja@gmail.com>
2026-07-20 22:24:15 +0900

    げんじぶ（曲）歌割り

--- src/data/songs/_template.js
+++ src/data/songs/_template.js
@@ -1,33 +1,27 @@
 export const song_template = {
-    id: "song_template", /* idを書く */
-    title: "曲名", /* 曲名を書く */
-    note: "", /* 注釈 */
+  id: "song_template" /* idを書く */,
+  note: "" /* 注釈 */,
 
-    /* 歌詞全体 */
-    lyrics_blocks: [
+  /* 歌詞全体 */
+  lyrics_blocks: [
+    {
+      /* ブロック分け */
+      rows: [
         {
-            /* ブロック分け */
-            rows: [
-                {
-                    /* 強制改行 */
-                    chunks: [
-                        /* 任意改行 */
-                        { /* 通常パターン */
-                            members: [""],
-                            text: [
-                                { lyric: "歌詞テキスト" },
-                            ],
-                        },
-                        { /* コールパターン */
-                            members: [""],
-                            text: [
-                                { lyric: "歌詞テキスト"},
-                                { lyric: "歌詞テキスト", call: true },
-                            ],
-                        },
-                    ],
-                },
-            ],
+          /* 強制改行 */
+          chunks: [
+            /* 任意改行 */
+            { members: [""], text: [{ lyrics: "歌詞テキスト" }] },
+            {
+              /* コールパターン */ members: [""],
+              text: [
+                { lyrics: "歌詞テキスト" },
+                { lyrics: "歌詞テキスト", call: true },
+              ],
+            },
+          ],
         },
-    ],
-}
+      ],
+    },
+  ],
+};
---
+++ src/data/songs/song001.js
@@ -0,0 +1,331 @@
+/* 原因は自分にある。 */
+
+export const song001 = {
+    id: "song001",
+    note: "デビュー曲。",
+
+    lyrics_blocks: [
+        {
+            rows: [
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "練習はOKです" },],},
+                        { text: [{ lyrics: "いやむしろ滑稽です" },],},
+                    ],
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "思考回路　雑　落ちるパラドックス" },],},
+                    ],
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "最上な回答で" },],},
+                        { text: [{ lyrics: "ワンチャン藻掻いたって" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { members: ["B"], text: [{ lyrics: "嘲笑の的です" },],},
+                        { members: ["S"], text: [{ lyrics: "（誰の声だ？）" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            members: ["Y"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "まだまだまだまだ足りない" },],},
+                        { text: [{ lyrics: "ねえダラダラダラダラしてたい" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "怠惰な僕ら　或いはそれはイデア" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "それともそもそも論外ですか？" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["W"],
+                    chunks: [
+                        { text: [{ lyrics: "高架下　現実逃避" },],},
+                        { text: [{ lyrics: "線と線で　描く" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { members: ["P"], text: [{ lyrics: "タイムラインで確かめるエラー" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { members: ["G"], text: [{ lyrics: "承認して" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "ちょちょちょっと懊悩 I don't know" },],},
+                        { text: [{ lyrics: "結局ダメだね" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "平和な青空　憎んだって" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "No No No　苦悩その残像" },],},
+                        { text: [{ lyrics: "そんなの知ってる" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "原因は自分にある。んだって" },],},
+                    ],
+                },
+                {
+                    members: ["W", "G"],
+                    chunks: [
+                        { text: [{ lyrics: "猶予　猶予" },],},
+                    ],
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "エゴサして　猫なでて" },],},
+                        { text: [{ lyrics: "オムライスでも食べて" },],},
+                    ],
+                },
+                {
+                    members: ["W", "G"],
+                    chunks: [
+                        { text: [{ lyrics: "猶予　猶予" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "最期まで召し上がれ" },],},
+                        { text: [{ lyrics: "沈黙で" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["W"],
+                    chunks: [
+                        { text: [{ lyrics: "感情を準備して" },],},
+                        { text: [{ lyrics: "平静の方向で" },],},
+                    ],
+                },
+                {
+                    members: ["W"],
+                    chunks: [
+                        { text: [{ lyrics: "視界がゆらゆらゆら揺れてるけど" },],},
+                    ],
+                },
+                {
+                    members: ["P"],
+                    chunks: [
+                        { text: [{ lyrics: "限界効用逓減" },],},
+                        { text: [{ lyrics: "何が欲しいの" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { members: ["P"], text: [{ lyrics: "それは言えない" },],},
+                        { members: ["S"], text: [{ lyrics: "（誰の声だ？）" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            members: ["B"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "でもでもでもでも足んない" },],},
+                        { text: [{ lyrics: "ほらカラカラカラカラ籠もる" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "咲いたら枯れるさ　ならば土の中" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "酸っぱい葡萄の種として眠ろう" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["G"],
+                    chunks: [
+                        { text: [{ lyrics: "「どうかした？」遠く一つ" },],},
+                        { text: [{ lyrics: "点とセンテンス　出会う" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { members: ["Y"], text: [{ lyrics: "タイムラインが止まるから" },],},
+                    ],
+                },
+                {
+                    chunks: [
+                        { members: ["S"], text: [{ lyrics: "帳消しで" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "ちょちょちょっと本能 I don't know" },],},
+                        { text: [{ lyrics: "せーので見せ合う" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "かまちょって何より勝ち負け" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "No No No　嘘つき参上" },],},
+                        { text: [{ lyrics: "せっかちでっかち" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "原因は自分にある。んだった" },],},
+                    ],
+                },
+                {
+                    members: ["W", "G"],
+                    chunks: [
+                        { text: [{ lyrics: "言うよ　言うよ" },],},
+                    ],
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "言い訳がしたいけれど" },],},
+                        { text: [{ lyrics: "ロスタイムなら影で" },],},
+                    ],
+                },
+                {
+                    members: ["W", "G"],
+                    chunks: [
+                        { text: [{ lyrics: "言うよ　言うよ" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "最低でも召し上がれ" },],},
+                        { text: [{ lyrics: "科学で" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            members: ["W"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "承認して" },],},
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "ちょちょちょっと懊悩 I don't know" },],},
+                        { text: [{ lyrics: "結局ダメだね" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "平和な青空　憎んだって" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "No No No　苦悩その残像" },],},
+                        { text: [{ lyrics: "そんなの知ってる" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "原因は自分にある。んだって" },],},
+                    ],
+                },
+                {
+                    members: ["W", "G"],
+                    chunks: [
+                        { text: [{ lyrics: "猶予　猶予" },],},
+                    ],
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "エゴサして　猫なでて" },],},
+                        { text: [{ lyrics: "オムライスでも食べて" },],},
+                    ],
+                },
+                {
+                    members: ["W", "G"],
+                    chunks: [
+                        { text: [{ lyrics: "猶予　猶予" },],},
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "最期まで召し上がれ" },],},
+                        { text: [{ lyrics: "沈黙で" },],},
+                    ],
+                },
+            ],
+        },
+    ],
+}
