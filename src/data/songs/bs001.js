43f1167e64a74a2b6453dc23e838674d802f9b04
observer-ja <observer.ja@gmail.com>
2026-07-21 21:13:26 +0900

    STARGAZER

---
+++ src/data/songs/bs001.js
@@ -0,0 +1,263 @@
+export const bs001 = {
+    id: "bs001",
+    note: "",
+
+    lyrics_blocks: [
+        {
+            members: ["Y"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "OK！　ルールなんて無いようなもん" }] },
+                        { text: [{ lyrics: "だからフリーにノッちゃっていい？" }] },
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "スクールなんざ内容はナッシング" }] },
+                        { text: [{ lyrics: "なコトでマジさっぱり" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            members: ["Y"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "そんなことより今夜のシャイニースター" }] },
+                        { text: [{ lyrics: "プラスキミに会いにきた" }] },
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "テキトーな　くらいで超ド良い" }] },
+                        { text: [{ lyrics: "ヤなコトなんて置き去り　Follow me, ya！" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            members: ["P"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "Rockしたキミの目" }] },
+                        { text: [{ lyrics: "縮めたいんだこのキョリ" }] },
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "どうか一緒にナイトフライトしないと" }] },
+                        { text: [{ lyrics: "タイトなタイム勿体無いよ" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            members: ["P"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "結果オーライでいい" }] },
+                        { text: [{ lyrics: "ほら、↑(上)に満天の星空" }] },
+                    ],
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "準備は出来た I gotta go" }] },
+                        { text: [{ lyrics: "Best timeでスタート Let's go" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "付いてきなよ即効フリーダム" }] },
+                        { text: [{ lyrics: "手放しエンジョイ最高潮" }] },
+                    ],
+                },
+                {
+                    members: [],
+                    chunks: [
+                        { text: [{ lyrics: "I'm STARGAZER" }] },
+                    ],
+                },
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "キラキラと輝く空" }]
+                        },
+                        {
+                            text: [
+                                { lyrics: "手を伸ばせばつかめそう", members: ["R"] },
+                                { lyrics: "On Now", members: [],}
+                            ]
+                        },
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "一緒にステップを決めたなら" }] },
+                        { text: [{ lyrics: "イマ Braze, Braze, Braze, Braze！" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            members: ["R"],
+            rows: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "キミだってきっと感じた" }] },
+                        { text: [{ lyrics: "果てしなくいける We can fly" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "当たり前だけどバッドマインドはNO" }] },
+                        { text: [{ lyrics: "言わなくたってきっとYou know" }] },
+                    ],
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "割り込みだって歓迎さ" }] },
+                        { text: [{ lyrics: "We're ドリーミーな雰囲気だろ？" }] },
+                    ],
+                },
+                {
+                    members: ["Y"],
+                    chunks: [
+                        { text: [{ lyrics: "からのやっぱシューティングしたいのはキミのハートでしょ！" }] },
+                    ],
+                },
+                {
+                    members: ["Y"],
+                    chunks: [
+                        { text: [{ lyrics: "ってか知ってる？" }] },
+                        { text: [{ lyrics: "ゼッタイCan't nobody hold us down" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "付いてきなよ即効フリーダム" }] },
+                        { text: [{ lyrics: "手放しエンジョイ最高潮" }] },
+                    ],
+                },
+                {
+                    members: [],
+                    chunks: [
+                        { text: [{ lyrics: "I'm STARGAZER" }] },
+                    ],
+                },
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "キラキラと輝く空" }]
+                        },
+                        {
+                            text: [
+                                { lyrics: "手を伸ばせばつかめそう", members: ["R"] },
+                                { lyrics: "On Now", members: []}
+                            ]
+                        },
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "一緒にステップを決めたなら" }] },
+                        { text: [{ lyrics: "イマ Braze, Braze, Braze, Braze！" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["P"],
+                    chunks: [
+                        { text: [{ lyrics: "Rock your body" }] },
+                        { text: [{ lyrics: "天体観測でStart in" }] },
+                    ],
+                },
+                {
+                    members: ["P"],
+                    chunks: [
+                        { text: [{ lyrics: "全てモンダイnothing" }] },
+                        { text: [{ lyrics: "キミが本当のstarlight" }] },
+                    ],
+                },
+                {
+                    members: ["Y"],
+                    chunks: [
+                        { text: [{ lyrics: "(O.K)" }] },
+                    ],
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "何の不安も無いよ" }] },
+                        { text: [{ lyrics: "だって一緒に見たいよ" }] },
+                        { text: [{ lyrics: "Show meスマイルを" }] },
+                    ],
+                },
+                {
+                    chunks: [
+                        {
+                            text: [
+                                { lyrics: "I, I, I, I,", members: [] },
+                                { lyrics: "I'm a GAZER", members: ["B"] }
+                            ]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "Can't nobody hold us down" }]
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "そう！そのテンション　Endless Fever" }] },
+                        { text: [{ lyrics: "どこまでもいけるフィーリングー" }] },
+                    ],
+                },
+                {
+                    members: [],
+                    chunks: [
+                        { text: [{ lyrics: "I'm STARGAZER" }] },
+                    ],
+                },
+                {
+                    members: ["R"],
+                    chunks: [
+                        { text: [{ lyrics: "キミだってきっと感じた" }] },
+                        { text: [{ lyrics: "果てしなくいける We can fly" }] },
+                    ],
+                },
+            ],
+        },
+    ],
+}
