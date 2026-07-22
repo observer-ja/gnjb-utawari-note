9475671306115d722b0d5a175b6cf8c8281f383c
observer-ja <observer.ja@gmail.com>
2026-07-22 17:30:19 +0900

    Burn Burn Bomb 途中

---
+++ src/data/songs/bs006.ts
@@ -0,0 +1,107 @@
+export const bs006 = {
+    id: "bs006",
+    note: "手で銃を作り、「Burn Burn Bomb Burn Bomb」に合わせて撃つ真似をする。",
+
+    lyrics_blocks: [
+        {
+            rows: [
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Burn Burn Bomb Burn Bomb x 3" }]
+                        },
+                        {
+                            members: ["B"],
+                            text: [{ lyrics: "張り裂けそうな My heart！" }]
+                        },
+                    ],
+                },
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Burn Burn Bomb Burn Bomb x 3" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "どうしようもなく Fire！" }]
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["W"],
+                    chunks: [
+                        { text: [{ lyrics: "まるで魔法　または催眠術のように" }] },
+                        { text: [{ lyrics: "体中に　電流が駆け巡っていった" }] },
+                    ],
+                },
+                {
+                    members: ["G"],
+                    chunks: [
+                        { text: [{ lyrics: "いつどこでも　キミのことが" }] },
+                        { text: [{ lyrics: "離れなくって" }] },
+                    ],
+                },
+                {
+                    members: ["G"],
+                    chunks: [
+                        { text: [{ lyrics: "どうしちゃったの！？" }] },
+                        { text: [{ lyrics: "どうなっちゃうの！？" }] },
+                        { text: [{ lyrics: "落ち着けないよ" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "ドラマ　映画　漫画　今まで見てたフィクション" }] },
+                        { text: [{ lyrics: "それが今や僕を悩ませるノン・フィクション！" }] },
+                    ],
+                },
+                {
+                    members: ["Y"],
+                    chunks: [
+                        { text: [{ lyrics: "Oh my heart beat　ヤバいぐらい" }] },
+                        { text: [{ lyrics: "Boom Boom　鳴ってる Burn Burn Bomb！" }] },
+                    ],
+                },
+            ],
+        },
+        {
+            rows: [
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Burn Burn Bomb Burn Bomb x 3" }]
+                        },
+                        {
+                            members: ["B"],
+                            text: [{ lyrics: "張り裂けそうな My heart！" }]
+                        },
+                    ],
+                },
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Burn Burn Bomb Burn Bomb x 3" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "どうしようもなく Fire！" }]
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+}
