9cf195a50b293178074745cfc0c7987413caf300
observer-ja <observer.ja@gmail.com>
2026-07-22 16:37:38 +0900

    更新情報欄

---
+++ src/data/news.ts
@@ -0,0 +1,12 @@
+export interface NewsItemType {
+    date: string;
+    text: string;
+    url?: string;
+}
+
+export const newsData: NewsItemType[] = [
+    {
+        date: "2026.08.??",
+        text: "サイトをオープンしました。"
+    },
+];
