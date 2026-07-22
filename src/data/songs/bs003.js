c72878332e351501727d60135c3515b0c1f031cc
observer-ja <observer.ja@gmail.com>
2026-07-22 11:44:57 +0900

    Macanas.

---
+++ src/data/songs/bs003.js
@@ -0,0 +1,324 @@
+export const bs003 = {
+    id: "bs003",
+    note: "",
+
+    lyrics_blocks: [
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "Na na na night" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "ハイ！　ハイ！　ハイ！", call: true }]
+                        },
+                    ]
+                },
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "Na na na night" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "ハイ！　ハイ！　ハイ！", call: true }]
+                        },
+                    ]
+                },
+            ],
+        },
+        {
+            members: ["B"],
+            row: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "夜更け過ぎ　人知れずこの場所で" }] },
+                        { text: [{ lyrics: "繰り広げられる　The Carnival" }] },
+                    ]
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "耳の奥を刺激するKiller Sound" }] },
+                        { text: [{ lyrics: "No, Nobody can stop me" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "Na na na night" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "ハイ！　ハイ！　ハイ！", call: true }]
+                        },
+                    ]
+                },
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "Na na na night" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "ハイ！　ハイ！　ハイ！", call: true }]
+                        },
+                    ]
+                },
+            ],
+        },
+        {
+            members: ["P"],
+            row: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "踊って、歌って　自分流" }] },
+                        { text: [{ lyrics: "どこまでいけるか　一晩中" }] },
+                    ]
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "I'm Going Going Baby" }] },
+                        { text: [{ lyrics: "I'm Going Going 永遠" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            members: ["Y"],
+            row: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "Feel so nice, Like a paradise" }] },
+                        { text: [{ lyrics: "止められない　心　体" }] },
+                    ]
+                },
+                {
+                    chunks: [
+                        { text: [{ lyrics: "裸足で　そう　ステップして" }] },
+                        { text: [{ lyrics: "Hey, Turn it up, Turn it up, Dance！" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Macanas momie qui las calle co" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "めくるめく魅惑の音に溺れよう" }]
+                        },
+                    ]
+                },
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Macanas momie qui las calle co" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "合わせた手のひらを叩き　始めよう" }]
+                        },
+                    ]
+                },
+                {
+                    members: ["P"],
+                    chunks: [
+                        { text: [{ lyrics: "Hola" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "Na na na night" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "ハイ！　ハイ！　ハイ！", call: true }]
+                        },
+                    ]
+                },
+                {
+                    chunks: [
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "Na na na night" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "ハイ！　ハイ！　ハイ！", call: true }]
+                        },
+                    ]
+                },
+            ],
+        },
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: ["Y"],
+                            text: [{ lyrics: "俺の鼓膜を支配する弾ける音の誓い" }]
+                        },
+                        {
+                            members: ["Y"],
+                            text: [{ lyrics: "すぐ" }]
+                        },
+                        {
+                            members: ["P"],
+                            text: [{ lyrics: "そこで光る未来が手招きしてるTonight" }]
+                        },
+                    ]
+                },
+            ],
+        },
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: ["B"],
+                            text: [{ lyrics: "この先どんな事が起ころうとも" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "We don't care, It's okay" }]
+                        },
+                    ]
+                },
+                {
+                    chunks: [
+                        {
+                            members: ["B"],
+                            text: [{ lyrics: "この音さえあれば何も要らない" }]
+                        },
+                        {
+                            members: [],
+                            text: [{ lyrics: "We will get, It's okay" }]
+                        },
+                    ]
+                },
+            ],
+        },
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Macanas momie qui las calle co" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "廻れ、廻れ　音の渦に溺れよう" }]
+                        },
+                    ]
+                },
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Macanas momie qui las calle co" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "後戻り出来ない夜に Bien venidos" }]
+                        },
+                    ]
+                },
+                {
+                    members: ["B"],
+                    chunks: [
+                        { text: [{ lyrics: "Hola" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            members: ["B"],
+            row: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "Yeah　やっと見つけた" }] },
+                        { text: [{ lyrics: "織りなすMelodyと弾むうねるBeat" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            members: ["R"],
+            row: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "もう聞きたくないんだ" }] },
+                        { text: [{ lyrics: "嘘にまみれた浮世のDirty sound" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            row: [
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Macanas momie qui las calle co" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "めくるめく魅惑の音に溺れよう" }]
+                        },
+                    ]
+                },
+                {
+                    chunks: [
+                        {
+                            members: [],
+                            text: [{ lyrics: "Macanas momie qui las calle co" }]
+                        },
+                        {
+                            members: ["R"],
+                            text: [{ lyrics: "合わせた手のひらを叩き　始めよう" }]
+                        },
+                    ]
+                },
+                {
+                    members: ["P"],
+                    chunks: [
+                        { text: [{ lyrics: "Hola" }] },
+                    ]
+                },
+            ],
+        },
+        {
+            members: [],
+            row: [
+                {
+                    chunks: [
+                        { text: [{ lyrics: "ハイ！　ハイ！　ハイ！　ハイ！", call: true }] },
+                    ]
+                },
+            ],
+        },
+    ],
+}
