218e03782ac2c8bbb69b586e5cc09d6d03aecb1c
observer-ja <observer.ja@gmail.com>
2026-08-09 21:12:02 +0900

    できた

--- src/data/songs/song003_7.ts
+++ src/data/songs/song003_7.ts
@@ -27,7 +27,7 @@ export interface SongLyrics {
 
 export const song003_7: SongLyrics = {
     id: "song003_7",
-    note: "5th Anniversary LIVE 夢現の続きでのみ披露された。",
+    note: "7人ver.は5th Anniversary LIVE 夢現の続きでのみ披露された。",
 
     lyrics_blocks: [
         {
